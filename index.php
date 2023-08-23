3893ae8ba8fd944d19767a20e413404a35d627f3
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-08-23 16:01:16 +0200

    initial commmit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!--style css-->
+  <link rel="stylesheet" href="./CSS/style.css">
+  <title>PHP Strong Password Generator</title>
+</head>
+<body>
+  <h1>
+    ok
+  </h1>
+  
+</body>
+</html>
