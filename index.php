e0f1930c358e022d9c449f86831dc2d736b1a58a
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-08-23 18:51:30 +0200

    add form input and function for generation random password (end milestone 1)

--- index.php
+++ index.php
@@ -1,16 +1,127 @@
+<?php
+  function passwordGenerator($passLength, $passWhitNumbers, $passWhitLetters, $passWhitSpecials) {
+    $randomPassword = '';
+    $charsNumber ='0123456789';
+    $charsLetters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charsSpecials ='!@#$%^&*()';
+    $charsAccepted = '';
+    if($passWhitNumbers && $passWhitLetters == false && $passWhitSpecials == false){
+
+      $charsAccepted .= $charsNumber;
+    
+    }elseif($passWhitNumbers == false && $passWhitLetters && $passWhitSpecials == false){
+    
+      $charsAccepted .= $charsLetters;
+    
+    }elseif($passWhitNumbers == false && $passWhitLetters == false && $passWhitSpecials ){
+    
+      $charsAccepted .= $charsSpecials;
+    
+    }elseif($passWhitNumbers && $passWhitLetters && $passWhitSpecials == false){
+    
+      $charsAccepted .= $charsNumber .= $charsLetters;
+    
+    }elseif($passWhitNumbers == false && $passWhitLetters && $passWhitSpecials ){
+    
+      $charsAccepted .= $charsLetters .= $charsSpecials;
+    
+    }elseif($passWhitNumbers  && $passWhitLetters == false && $passWhitSpecials ){
+    
+      $charsAccepted .=  $charsNumber .= $charsSpecials;
+    
+    }elseif($passWhitNumbers && $passWhitLetters && $passWhitSpecials){
+    
+      $charsAccepted .= $charsNumber .= $charsLetters .= $charsSpecials;
+    
+    }
+
+    while (strlen($randomPassword) < $passLength) {
+      $randomIndex = rand(0, strlen($charsAccepted) - 1);
+
+      if(!strpos($randomPassword, $charsAccepted[$randomIndex])){
+        $randomPassword .= $charsAccepted[$randomIndex];
+      }
+    }
+
+
+
+    return [
+            'password' => $randomPassword, 
+            'maxValueAcepted' => strlen($charsAccepted), 
+          ];
+  };
+
+  if(isset($_GET['passwordLength'])){
+    $passwordLength = (int) $_GET['passwordLength'];
+    $passWhitNumbers = true;
+    $passWhitLetters = true;
+    $passWhitSpecials = true;
+    $randomPassword = passwordGenerator($passwordLength,$passWhitNumbers, $passWhitLetters, $passWhitSpecials );
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="it">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!--bootstrap-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
   <!--style css-->
   <link rel="stylesheet" href="./CSS/style.css">
   <title>PHP Strong Password Generator</title>
 </head>
-<body>
-  <h1>
-    ok
-  </h1>
-  
+<body class="bg-primary bg-gradient vh-100">
+  <div class="container">
+    <h1 class="text-center pt-5">Strong Password Generator</h1>
+    <h2 class="text-center mt-2">Genera una password sicura </h2>
+
+    <p class="mt-5 bg-info bg-opacity-50 px-2 py-3 rounded text-white fw-bold">
+      Password Generata :
+      <span class="text-danger">
+        <?php 
+          if(isset($passwordLength)){
+            echo $randomPassword['password'];
+          }else{
+            echo 'nessun parametro valido inserito';
+          }
+        
+        ?>
+      </span>
+      
+    </p>
+
+    <div class="row">
+      <div class="coll">
+        <form class="mt-5 bg-white p-3 rounded" action="index.php" method="get">
+          <div class="mb-3">
+            <label for="passwordLength" class="form-label">
+              Lunghezza password:  
+            </label>
+            <input 
+              type="number" 
+              class="form-control" 
+              id="passwordLength" 
+              name="passwordLength" 
+              min="0"
+              max="<?=$randomPassword['maxValueAcepted'] ?? '72'?>" 
+              value="<?php 
+                  if(isset($passwordLength)){
+                    echo (string) $passwordLength;
+                  }
+              ?>"
+            >
+          </div>
+          <button type="submit" class="btn btn-primary">
+            Genera Password
+          </button>
+          <button type="button" class="btn btn-secondary">
+            Annulla
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
+
 </body>
 </html>
